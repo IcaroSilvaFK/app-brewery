f26e5e7a314c50b6058098f0b5ff4e0ae59c5b30
Icaro Silva <iv2484706@gmail.com>
2023-08-20 13:25:04 -0300

    Initial Commit

---
+++ app-brewery/app_breweryApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_breweryApp.swift
+//  app-brewery
+//
+//  Created by Icaro on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct app_breweryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
