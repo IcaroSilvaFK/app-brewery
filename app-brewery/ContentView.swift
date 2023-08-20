207f3fe6f7002d1e903a54eb5522be5f273c6449
Icaro Silva <iv2484706@gmail.com>
2023-08-20 13:29:56 -0300

    init

--- app-brewery/ContentView.swift
+++ app-brewery/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Olá mundo")
+            Text("Hello world")
         }
         .padding()
     }
